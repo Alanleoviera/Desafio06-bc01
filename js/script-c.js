773944725f9f391305da2cb5086f35c78fe3ad41
Victor Javier <vjgenez56@gmail.com>
2024-01-28 22:52:39 -0300

    Resolvi ejercicio C

--- js/script-c.js
+++ js/script-c.js
@@ -1,7 +1,20 @@
 
 console.log('----------Ejercicio C----------')    // mensaje por consola que indique la letra del ejercicio
 
+var cantidadDeGatos = 8;
+var cantidadDePasos = 5;
 
+var gato = '🐈';
+var gatoNegro = '🐈‍⬛';
+var pasos = '🐾';
+
+for (n = 1 ; cantidadDeGatos>=n ; n++) {
+   var emoji = n%2 === 1 ? gato : gatoNegro;
+
+    var resultado = ('Gato #' + n + ': ' + emoji + ' ' + pasos.repeat(cantidadDePasos))
+
+    console.log (resultado)
+}
 
 
 
