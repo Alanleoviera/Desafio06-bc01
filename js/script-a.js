c63278b595748399f11d22758b9c142d620d261e
Alan <alanleoviera@gmail.com>
2024-01-28 21:43:05 -0300

    Resolví y subí el ejercicio A

--- js/script-a.js
+++ js/script-a.js
@@ -1,4 +1,19 @@
 
 console.log('----------Ejercicio A----------')    // mensaje por consola que indique la letra del ejercicio
 
+var cantidadDeGatos = 15; // valor que se puede cambiar
+var emoji = ''; 
 
+for (var n = 1; cantidadDeGatos >= n ; n++) {
+
+    // Utilizar el operador de módulo para alternar entre los 3 emojis
+    if (n % 3 === 1) {
+        emoji = '😺';
+    } else if (n % 3 === 2) {
+        emoji = '😸';
+    } else {
+        emoji = '😹';
+    }
+
+    console.log('Gato #' + n + ': ' + emoji);
+}
