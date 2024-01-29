9d29ad300dded1aaa738024f91d49ddc4216ec97
Yess <yessicarint@gmail.com>
2024-01-28 22:19:05 -0300

    Resolvi el Ejercicio B

--- js/script-b.js
+++ js/script-b.js
@@ -1,8 +1,18 @@
 
 console.log('----------Ejercicio B----------')    // mensaje por consola que indique la letra del ejercicio
 
+var cantidadDeGatos = 10;
+var cantidadDePasos = 4;
 
+var gato = ('🐈');
+var pasos =('🐾');
 
+for (var n = 1; cantidadDeGatos >= n ; n++) {
+ 
+    var resultado = ('Gato #' + n + ': ' + gato + ' ' + pasos.repeat(cantidadDePasos));
+
+    console.log(resultado);
+}
 
 
 
